1da4e6d210f68b329268deeb4ddb9b17223cc941
ian momyer <imomyer@gmail.com>
2023-03-31 18:01:51 -0400

    init

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `The Garden Spot`,
+    siteUrl: `thegardenspot.nyc`
+  },
+  plugins: ["gatsby-plugin-google-gtag"]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      THE GARDEN SPOT
+    </main>
+  )
+}
+
+export default IndexPage
