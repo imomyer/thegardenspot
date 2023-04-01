51d6abf43ce40a92a00b0a73c8f5a8769967c6a6
ian momyer <imomyer@gmail.com>
2023-04-01 14:36:09 -0400

    first design

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@ module.exports = {
     title: `The Garden Spot`,
     siteUrl: `https://thegardenspot.nyc`
   },
-  plugins: ["gatsby-plugin-netlify"],
+  plugins: ["gatsby-plugin-netlify", {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Courier Prime`,
+            file: `https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap`,
+          },
+        ],
+      },
+    },],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import * as React from "react"
 
+import '../styles/global.css'
+
 const IndexPage = () => {
   return (
-    <main style={{textAlign: 'center'}}>
+    <main>
       THE GARDEN SPOT
+      <img
+      src="../..//Roll_310-27.jpg"
+      alt="The Garden Spot"
+    />
     </main>
   )
 }
