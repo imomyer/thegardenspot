a6b23397f434021bfcef7fe753867a865cda3ad1
ian momyer <imomyer@gmail.com>
2023-04-14 14:41:45 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,9 @@ module.exports = {
     title: `The Garden Spot`,
     siteUrl: `https://thegardenspot.nyc`
   },
-  plugins: ["gatsby-plugin-netlify", {
+  plugins: [
+    "gatsby-plugin-netlify", 
+    {
       resolve: `gatsby-omni-font-loader`,
       options: {
         enableListener: true,
@@ -18,5 +20,35 @@ module.exports = {
           },
         ],
       },
-    },],
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-1YY99W547Y", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
+  ],
 };
