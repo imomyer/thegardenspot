71ecb70042c8399dc904dc545badbb2f614e42d9
ian momyer <imomyer@gmail.com>
2023-06-02 08:22:09 -0400

    add 299-022

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,13 @@ const IndexPage = () => {
       <div class="content">
         <div>
             <img
+          class="landscape"
+          src="../../Roll_299-22.jpg"
+          alt="The Garden Spot"
+        />
+        </div>
+        <div>
+            <img
           class="portrait"
           src="../../Roll_309-032.jpg"
           alt="The Garden Spot"
