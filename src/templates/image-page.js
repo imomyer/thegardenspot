ea59fadf7c67327a3b49f050a25033da54d4d617
ian momyer <imomyer@gmail.com>
2024-12-21 17:02:00 -0500

    add support for projects

--- src/templates/image-page.js
+++ src/templates/image-page.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 
-const GeneralImagePage = ({data}) => {
-    const image = data.imagesJson;
+const GeneralImagePage = ({data, pageContext}) => {
+    const item = data.imagesJson;
+    let image = data.imagesJson;
+    let imagePath = `../../${image.fileName}`;
+    let imageTitle = image.title;
+    let imageOrientation = image.orientation;
+
+    console.log(imageOrientation)
+
+    if (item.images) {
+      image = data.imagesJson.images.filter((image) => image.slug === pageContext.slug)[0];
+      imagePath = `../../../${image.fileName}`;
+      imageOrientation = image.orientation;
+    }
+
     return (
       <main className="image-page">
         <div className="title-container"><h1>THE GARDEN SPOT</h1><span><Link to='/'>back</Link></span></div>
         <div className="content-container">
           <div className="image-container">
             <img
-              src={`../../${image.fileName}`}
-              alt={image.title}
+              src={imagePath}
+              alt={imageTitle}
+              className={imageOrientation}
             />
           </div>
         </div>
@@ -19,10 +33,19 @@ const GeneralImagePage = ({data}) => {
 }
 
 export const query = graphql`
-  query ($slug: String!) {
-    imagesJson(slug: { eq: $slug }) {
+  query ($itemID: String!) {
+    imagesJson(itemID: { eq: $itemID }) {
+      itemID
       title
       fileName
+      project
+      orientation
+      images {
+        title
+        slug
+        fileName
+        orientation
+      }
     }
   }
 `;
